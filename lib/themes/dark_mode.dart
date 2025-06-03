7a18985bee8c210e121d41a3eb17e8aa7e670cc1
Víctor Crusat <vcrusat.one@gmail.com>
2025-06-03 22:46:10 +0200

    Added light and dark themes

---
+++ lib/themes/dark_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkModeTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+      primary: Colors.grey.shade500,
+      secondary: Colors.grey.shade700,
+      tertiary: Colors.grey.shade900,
+      inversePrimary: Colors.grey.shade300),
+  scaffoldBackgroundColor: Colors.grey.shade800,
+);
